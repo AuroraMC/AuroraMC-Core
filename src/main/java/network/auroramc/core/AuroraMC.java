9794746ec7579b417ea7844a055b9b43dd1ff859
Ethan P-B <accounts@block2block.me>
2020-04-10 21:41:12 +0100

    Initial commit

---
+++ src/main/java/network/auroramc/core/AuroraMC.java
@@ -0,0 +1,16 @@
+package network.auroramc.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuroraMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
