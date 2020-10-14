e5260a0ad305df8dd67e9534a91aba4d4e0ad264
Heliology <burntcbusiness@gmail.com>
2020-10-14 19:03:18 +0100

    Created SocialMedia SubRank & Renamed Social Permission
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/network/auroramc/core/permissions/permissions/SocialMedia.java
+++ src/main/java/network/auroramc/core/permissions/permissions/SocialMedia.java
@@ -1,11 +1,10 @@
 package network.auroramc.core.permissions.permissions;
 
 import network.auroramc.core.api.permissions.Permission;
+import org.jetbrains.annotations.NotNull;
 
-public final class SocialMedia extends Permission {
-
+public class SocialMedia extends Permission {
     public SocialMedia() {
-        super(11, "social");
+        super(15, "socialmedia");
     }
-
 }
