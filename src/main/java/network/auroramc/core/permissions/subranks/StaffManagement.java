d576402798bd2e536a8bf45d1c806e3211733513
Heliology <burntcbusiness@gmail.com>
2020-10-15 16:34:55 +0100

    Give Staff Management access to /disguise
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/network/auroramc/core/permissions/subranks/StaffManagement.java
+++ src/main/java/network/auroramc/core/permissions/subranks/StaffManagement.java
@@ -5,10 +5,11 @@ import network.auroramc.core.api.permissions.SubRank;
 import org.bukkit.Color;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 public final class StaffManagement extends SubRank {
     public StaffManagement() {
-        super(4, "Staff Management", new ArrayList<>(Collections.singletonList(AuroraMCAPI.getPermissions().get("staffmanagement"))), Color.fromRGB(255, 170, 0), '6');
+        super(4, "Staff Management", new ArrayList<>(Arrays.asList(AuroraMCAPI.getPermissions().get("staffmanagement"), AuroraMCAPI.getPermissions().get("disguise"))), Color.fromRGB(255, 170, 0), '6');
     }
 }
