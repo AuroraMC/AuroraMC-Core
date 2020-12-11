ecd2f8881d9c6867b043dff3339c4cb85bd8b36e
Block2Block <accounts@block2block.me>
2020-12-11 22:14:40 +0000

    Slight cosmetic changes
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/commands/moderation/CommandPunishmentLookup.java
+++ src/main/java/net/auroramc/core/commands/moderation/CommandPunishmentLookup.java
@@ -25,7 +25,7 @@ public class CommandPunishmentLookup extends Command {
         if(args.size() == 1) {
             if(args.get(0).matches("[A-Z0-9]{8}")) {
                 String code = args.remove(0);
-                player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Punish", String.format("Performing punishment lookup for [**%s**]", code)));
+                player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Punish", String.format("Performing punishment lookup for [**%s**]...", code)));
                 new BukkitRunnable() {
                     @Override
                     public void run() {
@@ -41,7 +41,7 @@ public class CommandPunishmentLookup extends Command {
                                     "Issued at: **%s**\n" +
                                     "Length: **%s**\n" +
                                     "Issued By: **%s**\n" +
-                                    "Status: %s&r%s", code, name, ((punishment.getStatus() == 7)?"Warning":((rule.getType() == 1)?"Chat Offence":((rule.getType() == 2)?"Game Offence":"Misc Offence"))), rule.getRuleName() + " - " + punishment.getExtraNotes(), weights[rule.getWeight()-1], new Date(punishment.getIssued()), ((punishment.getExpire() == -1)?new PunishmentLength(-1):new PunishmentLength((punishment.getExpire() - punishment.getIssued()) / 3600000d)), punishment.getPunisherName(), statuses[punishment.getStatus()-1], ((punishment.getRemovalReason() == null)?"":String.format("\n\nRemoval Reason: **%s**\nRemoval Timestamp: **%s**\nRemoved By: **%s**", punishment.getRemovalReason(), new Date(punishment.getRemovalTimestamp()), punishment.getRemover())))));
+                                    "Status: %s&r%s", code, name, ((punishment.getStatus() == 7)?"Warning":((rule.getType() == 1)?"Chat Offence":((rule.getType() == 2)?"Game Offence":"Misc Offence"))), rule.getRuleName() + " - " + punishment.getExtraNotes(), weights[rule.getWeight()-1], new Date(punishment.getIssued()), ((punishment.getExpire() == -1)?new PunishmentLength(-1):new PunishmentLength((punishment.getExpire() - punishment.getIssued()) / 3600000d)), punishment.getPunisherName(), statuses[punishment.getStatus()-1], ((punishment.getRemovalReason() == null)?"":String.format("\n \nRemoval Reason: **%s**\nRemoval Timestamp: **%s**\nRemoved By: **%s**", punishment.getRemovalReason(), new Date(punishment.getRemovalTimestamp()), punishment.getRemover())))));
                         } else {
                             player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Punish", String.format("No matches found for Punishment ID: [**%s**]", code)));
                         }
