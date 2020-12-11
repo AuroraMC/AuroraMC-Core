f0f5df7d0666f76ca0c1314827ce1251b76c40ec
Heliology <burntcbusiness@gmail.com>
2020-12-11 01:25:10 +0000

    Fix Appeal Command Bugs
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/net/auroramc/core/commands/moderation/qualityassurance/CommandAppeal.java
+++ src/main/java/net/auroramc/core/commands/moderation/qualityassurance/CommandAppeal.java
@@ -29,12 +29,12 @@ public class CommandAppeal extends Command {
                             if(punishment.getStatus() == 1 || punishment.getStatus() == 3) {
                                 UUID uuid = AuroraMCAPI.getDbManager().getUUIDFromID(punishment.getPunished());
                                 List<Punishment> punishments = AuroraMCAPI.getDbManager().getPunishmentHistory(punishment.getPunished());
-                                if (args.get(2).equals("Reprieve")) {
+                                if (args.get(0).equals("Reprieve")) {
                                     AuroraMCAPI.getDbManager().removePunishment("AuroraMCAppeals", System.currentTimeMillis(), "Reprieve", punishment, uuid, punishments);
-                                    AuroraMCAPI.getFormatter().pluginMessage("Appeal", String.format("Reprieved Punishment with ID [**%s**] has been removed successfully."));
-                                } else if (args.get(2).equals("False")) {
+                                    player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Appeal", String.format("Reprieved Punishment with ID [**%s**] has been removed successfully.", code)));
+                                } else if (args.get(0).equals("False")) {
                                     AuroraMCAPI.getDbManager().removePunishment("AuroraMCAppeals", System.currentTimeMillis(), "False", punishment, uuid, punishments);
-                                    AuroraMCAPI.getFormatter().pluginMessage("Appeal", String.format("False Punishment with ID [**%s**] has been removed successfully."));
+                                    player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Appeal", String.format("False Punishment with ID [**%s**] has been removed successfully.", code)));
                                 } else {
                                     player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Appeal", "Invalid syntax. Correct syntax: **/appeal [Punishment ID] [Reprieve | False]"));
                                 }
