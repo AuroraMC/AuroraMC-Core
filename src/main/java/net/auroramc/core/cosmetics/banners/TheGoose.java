c7b0f7d5277409fe1e23fe89834db9f4e8f9cd50
Heliology <32965968+Heliology@users.noreply.github.com>
2021-01-23 18:03:03 +0000

    Add Honk & Goose banners & fix the description of Angry Craig
    Signed-off-by: Heliology <32965968+Heliology@users.noreply.github.com>

---
+++ src/main/java/net/auroramc/core/cosmetics/banners/TheGoose.java
@@ -0,0 +1,28 @@
+package net.auroramc.core.cosmetics.banners;
+
+import net.auroramc.core.api.cosmetics.Banner;
+import org.bukkit.DyeColor;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.block.banner.PatternType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TheGoose extends Banner {
+
+    public static final List<Pattern> patterns;
+
+    static {
+        patterns = new ArrayList<>();
+        patterns.add(new Pattern(DyeColor.ORANGE, PatternType.MOJANG));
+        patterns.add(new Pattern(DyeColor.WHITE, PatternType.CURLY_BORDER));
+        patterns.add(new Pattern(DyeColor.WHITE, PatternType.HALF_HORIZONTAL));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.STRIPE_TOP));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.STRIPE_DOWNRIGHT));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.HALF_VERTICAL_MIRROR));
+    }
+
+    public TheGoose() {
+        super(5, "The Goose", "&fThe&r &6Goose", "&fPeace was never an option.", UnlockMode.CRATE, -1, new ArrayList<>(), new ArrayList<>(), patterns, DyeColor.CYAN);
+    }
+}
