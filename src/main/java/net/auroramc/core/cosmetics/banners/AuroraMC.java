a0c13f0311cc4681841731a1f987f493f99dfa69
Heliology <32965968+Heliology@users.noreply.github.com>
2021-01-23 16:26:24 +0000

    Added 3 more banners.
    Signed-off-by: Heliology <32965968+Heliology@users.noreply.github.com>

---
+++ src/main/java/net/auroramc/core/cosmetics/banners/AuroraMC.java
@@ -0,0 +1,27 @@
+package net.auroramc.core.cosmetics.banners;
+
+import net.auroramc.core.api.cosmetics.Banner;
+import org.bukkit.DyeColor;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.block.banner.PatternType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuroraMC extends Banner {
+
+    public static final List<Pattern> patterns;
+
+    static {
+        patterns = new ArrayList<>();
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.STRIPE_RIGHT));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.STRIPE_LEFT));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.STRIPE_TOP));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.STRIPE_MIDDLE));
+        patterns.add(new Pattern(DyeColor.CYAN, PatternType.BORDER));
+    }
+
+    public AuroraMC() {
+        super(3, "AuroraMC?", "&3AuroraMC?", "&3It's the best we could do, okay?", UnlockMode.CRATE, -1, new ArrayList<>(), new ArrayList<>(), patterns, DyeColor.WHITE);
+    }
+}
---
+++ src/main/java/net/auroramc/core/cosmetics/banners/UnitedStates.java
@@ -0,0 +1,31 @@
+package net.auroramc.core.cosmetics.banners;
+
+import net.auroramc.core.api.cosmetics.Banner;
+import org.bukkit.DyeColor;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.block.banner.PatternType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnitedStates extends Banner {
+
+    private static final List<Pattern> patterns;
+
+    static {
+        patterns = new ArrayList<>();
+        patterns.add(new Pattern(DyeColor.WHITE, PatternType.STRIPE_SMALL));
+        patterns.add(new Pattern(DyeColor.WHITE, PatternType.STRIPE_SMALL));
+        patterns.add(new Pattern(DyeColor.WHITE, PatternType.STRIPE_SMALL));
+        patterns.add(new Pattern(DyeColor.BLUE, PatternType.SQUARE_TOP_RIGHT));
+        patterns.add(new Pattern(DyeColor.BLUE, PatternType.SQUARE_TOP_RIGHT));
+        patterns.add(new Pattern(DyeColor.BLUE, PatternType.SQUARE_TOP_RIGHT));
+        patterns.add(new Pattern(DyeColor.BLUE, PatternType.SQUARE_TOP_RIGHT));
+        patterns.add(new Pattern(DyeColor.BLUE, PatternType.SQUARE_TOP_RIGHT));
+    }
+
+    public UnitedStates() {
+        super(1, "United States Flag", "&cUnited States Flag", "&cUSA! USA! USA!", UnlockMode.CRATE, -1, new ArrayList<>(), new ArrayList<>(), patterns, DyeColor.RED);
+    }
+
+}
