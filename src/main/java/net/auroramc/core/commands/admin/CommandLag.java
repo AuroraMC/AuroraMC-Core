65f96a8634a3b8db7288df5d227ccc2e242b96f4
Block2Block <accounts@block2block.me>
2021-02-18 16:36:13 +0000

    Expanded /lag slightly
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/commands/admin/CommandLag.java
+++ src/main/java/net/auroramc/core/commands/admin/CommandLag.java
@@ -4,6 +4,7 @@ import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.core.api.command.Command;
 import net.auroramc.core.api.players.AuroraMCPlayer;
 import net.minecraft.server.v1_8_R3.MinecraftServer;
+import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 import java.lang.management.ManagementFactory;
@@ -23,13 +24,17 @@ public class CommandLag extends Command {
     @Override
     public void execute(AuroraMCPlayer player, String aliasUsed, List<String> args) {
         MemoryMXBean bean = ManagementFactory.getMemoryMXBean();
+        int threadsCurrentlyExecuting = Bukkit.getScheduler().getActiveWorkers().size();
+        int threadsAwaitingExecution = Bukkit.getScheduler().getPendingTasks().size();
         player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Resource Monitor", String.format("Server lag statistics:\n" +
                 "Used RAM: **%sMB**\n" +
                 "Total RAM: **%sMB**\n" +
+                "Threads Executing: **%s**\n" +
+                "Threads Scheduled: **%s**\n" +
                 "TPS (1m, 5m, 15m): **%s**, **%s**, **%s**\n" +
                 "Stored values:\n" +
                 " - Players: **%s**\n" +
-                " - Open GUI's: **%s**", bean.getHeapMemoryUsage().getUsed()/1048576,Runtime.getRuntime().maxMemory()/1048576, new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[0]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[1]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[2]), AuroraMCAPI.getPlayers().size(), AuroraMCAPI.getOpenGUIs())));
+                " - Open GUI's: **%s**", bean.getHeapMemoryUsage().getUsed()/1048576,Runtime.getRuntime().maxMemory()/1048576, threadsCurrentlyExecuting, threadsAwaitingExecution, new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[0]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[1]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[2]), AuroraMCAPI.getPlayers().size(), AuroraMCAPI.getOpenGUIs())));
     }
 
     @Override
