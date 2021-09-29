563187f2797a67aa86147812c0604ad809361b0c
Block2Block <accounts@block2block.me>
2021-09-29 19:40:26 +0100

    Made all cosmetics avail from crate only
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/cosmetics/banners/CandyCane.java
+++ src/main/java/net/auroramc/core/cosmetics/banners/CandyCane.java
@@ -25,6 +25,6 @@ public class CandyCane extends Banner {
     }
 
     public CandyCane() {
-        super(11, "Candy Cane", "&c&lCandy&r &f&lCane", "&cMmmm tasty...", UnlockMode.ALL, -1, new ArrayList<>(), new ArrayList<>(), "Found in Candy Crates", patterns, DyeColor.RED, true);
+        super(11, "Candy Cane", "&c&lCandy&r &f&lCane", "&cMmmm tasty...", UnlockMode.CRATE, -1, new ArrayList<>(), new ArrayList<>(), "Found in Candy Crates", patterns, DyeColor.RED, true);
     }
 }
