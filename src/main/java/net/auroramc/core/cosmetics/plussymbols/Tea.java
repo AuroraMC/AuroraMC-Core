6fb0ae9745e87f0d6a0fc527a35fb6856a59a011
BuildTools <unconfigured@null.spigotmc.org>
2021-09-30 20:59:15 +0100

    Add two new Plus Symbols

---
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/Tea.java
@@ -0,0 +1,14 @@
+package net.auroramc.core.cosmetics.plussymbols;
+
+import net.auroramc.core.api.cosmetics.PlusSymbol;
+import net.auroramc.core.api.permissions.Rank;
+import org.bukkit.Material;
+
+import java.util.Collections;
+
+public class Tea extends PlusSymbol {
+
+    public Tea() {
+        super(202, "Tea", "&3&l☕ Tea", "Show off how British you are with this Tea Plus Symbol!", UnlockMode.RANK, -1, Collections.emptyList(), Collections.singletonList(Rank.ELITE), "&bPurchase Elite at store.auroramc.net to unlock this Plus Symbol!", true, Material.GLASS_BOTTLE, (short)0, '☕');
+    }
+}
