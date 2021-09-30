c72b473026ba4d101abc79ee3ddab83e78827867
Heliology <burntcbusiness@gmail.com>
2021-09-30 21:32:05 +0100

    Change Trademark because it looked ugly.
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/Copyright.java
@@ -0,0 +1,14 @@
+package net.auroramc.core.cosmetics.plussymbols;
+
+import net.auroramc.core.api.cosmetics.PlusSymbol;
+import net.auroramc.core.api.permissions.Rank;
+import org.bukkit.Material;
+
+import java.util.Collections;
+
+public class Copyright extends PlusSymbol {
+
+    public Copyright() {
+        super(204, "Copyright", "&3&l© Copyright", "Coming Soon™", UnlockMode.RANK, -1, Collections.emptyList(), Collections.singletonList(Rank.ADMIN), "", false, Material.DIAMOND_AXE, (short)0, '©');
+    }
+}
