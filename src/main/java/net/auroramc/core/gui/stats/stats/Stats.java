6744e7e9333cca9a638afa54f1cbb3c6369a324c
Block2Block <accounts@block2block.me>
2021-10-03 01:47:40 +0100

    Fixed a bug
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/gui/stats/stats/Stats.java
+++ src/main/java/net/auroramc/core/gui/stats/stats/Stats.java
@@ -1,6 +1,7 @@
 package net.auroramc.core.gui.stats.stats;
 
 import net.auroramc.core.api.AuroraMCAPI;
+import net.auroramc.core.api.permissions.PlusSubscription;
 import net.auroramc.core.api.players.AuroraMCPlayer;
 import net.auroramc.core.api.stats.PlayerStatistics;
 import net.auroramc.core.api.utils.gui.GUI;
@@ -15,13 +16,15 @@ public class Stats extends GUI {
     private final AuroraMCPlayer player;
     private final String name;
     private final PlayerStatistics stats;
+    private final PlusSubscription subscription;
 
-    public Stats(AuroraMCPlayer player, String targetName, PlayerStatistics targetStatistics) {
+    public Stats(AuroraMCPlayer player, String targetName, PlayerStatistics targetStatistics, PlusSubscription subscription) {
         super("&3&l" + targetName + "'s Statistics", 5, true);
 
         this.player = player;
         this.name = targetName;
         this.stats = targetStatistics;
+        this.subscription = subscription;
 
         border(String.format("&3&l%s's Statistics", name), "");
         this.setItem(0, 4, new GUIItem(Material.SKULL_ITEM, String.format("&3&l%s's Statistics", name), 1, "", (short) 3, false, name));
@@ -32,7 +35,7 @@ public class Stats extends GUI {
     public void onClick(int row, int column, ItemStack item, ClickType clickType) {
         if (row == 1 && column == 4) {
             AuroraMCAPI.closeGUI(player);
-            GeneralStatistics stats = new GeneralStatistics(player, name, this.stats);
+            GeneralStatistics stats = new GeneralStatistics(player, name, this.stats, this.subscription);
             stats.open(player);
             AuroraMCAPI.openGUI(player, stats);
         } else {
