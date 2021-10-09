4d6bc3c8d5223fdba4099d12b99a6028319b0e08
Heliology <burntcbusiness@gmail.com>
2021-10-09 18:14:54 +0100

    Add (to be completed) help command
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

---
+++ src/main/java/net/auroramc/core/commands/general/CommandHelp.java
@@ -0,0 +1,27 @@
+package net.auroramc.core.commands.general;
+
+import net.auroramc.core.api.command.Command;
+import net.auroramc.core.api.permissions.Permission;
+import net.auroramc.core.api.players.AuroraMCPlayer;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CommandHelp extends Command {
+
+    public CommandHelp() {
+        super("help", Arrays.asList("helpme", "ineedhelp"), Collections.singletonList(Permission.PLAYER), false, null);
+    }
+
+    @Override
+    public void execute(AuroraMCPlayer player, String aliasUsed, List<String> args) {
+    }
+
+    @Override
+    public @NotNull List<String> onTabComplete(AuroraMCPlayer player, String aliasUsed, List<String> args, String lastToken, int numberArguments) {
+        return new ArrayList<>();
+    }
+}
