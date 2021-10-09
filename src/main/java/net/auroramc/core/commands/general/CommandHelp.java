b3d9841c69c5073f22eb3f0e3285103ec78abd06
Heliology <burntcbusiness@gmail.com>
2021-10-09 19:01:36 +0100

    Add two links to /help
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/net/auroramc/core/commands/general/CommandHelp.java
+++ src/main/java/net/auroramc/core/commands/general/CommandHelp.java
@@ -3,6 +3,11 @@ package net.auroramc.core.commands.general;
 import net.auroramc.core.api.command.Command;
 import net.auroramc.core.api.permissions.Permission;
 import net.auroramc.core.api.players.AuroraMCPlayer;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -12,12 +17,35 @@ import java.util.List;
 
 public class CommandHelp extends Command {
 
+    public String convert(@NotNull String message) {
+        return org.bukkit.ChatColor.translateAlternateColorCodes('&', message);
+    }
+
     public CommandHelp() {
         super("help", Arrays.asList("help", "helpme", "helpmeorsiawillcomeandmakeyouthegreatest", "helpmeorelseillkillyou", "helpwouldbeappreciated"), Collections.singletonList(Permission.PLAYER), false, null);
     }
 
     @Override
     public void execute(AuroraMCPlayer player, String aliasUsed, List<String> args) {
+        TextComponent textComponent = new TextComponent("");
+        TextComponent prefix = new TextComponent("«HELP»");
+        prefix.setColor(ChatColor.DARK_AQUA);
+        prefix.setBold(true);
+
+        textComponent.addExtra(prefix);
+
+        textComponent.addExtra(convert(String.format(" Click to be redirected to a help option.\n\n")));
+
+        TextComponent Rules = new TextComponent(" &b* AuroraMC Rules\n");
+        Rules.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, String.format("https://auroramc.net/rules")));
+        Rules.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to see our rules!").color(ChatColor.LIGHT_PURPLE).create()));
+        textComponent.addExtra(Rules);
+
+        TextComponent bugReport = new TextComponent(" &b* Report an issue with our systems\n");
+        bugReport.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, String.format("https://auroramc.net/bug-report/")));
+        bugReport.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Click here to report an issue with our systems!").color(ChatColor.LIGHT_PURPLE).create()));
+        textComponent.addExtra(bugReport);
+
     }
 
     @Override
