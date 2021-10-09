99f48ae6bffb3f26408b1083f8f32da4adcad390
Heliology <burntcbusiness@gmail.com>
2021-10-09 18:31:51 +0100

    Add some very long command names as aliases.
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/net/auroramc/core/commands/moderation/CommandStaffChat.java
+++ src/main/java/net/auroramc/core/commands/moderation/CommandStaffChat.java
@@ -18,7 +18,7 @@ public class CommandStaffChat extends Command {
 
 
     public CommandStaffChat() {
-        super("staffchat", Arrays.asList("sc", "s", "staff"), Collections.singletonList(Permission.PLAYER), false, null);
+        super("staffchat", Arrays.asList("sc", "s", "staff", "doyourjobmod", "helpmemoderatoryouresupposedtohelppeoplewhichiswhyyouramoderator"), Collections.singletonList(Permission.PLAYER), false, null);
     }
 
     @Override
