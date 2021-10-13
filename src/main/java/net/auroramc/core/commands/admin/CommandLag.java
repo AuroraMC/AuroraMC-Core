2857583fe4e905af300a969fa23b0cd15220b64e
BuildTools <unconfigured@null.spigotmc.org>
2021-10-13 11:28:29 +0100

    New additions to /lag
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/net/auroramc/core/commands/admin/CommandLag.java
+++ src/main/java/net/auroramc/core/commands/admin/CommandLag.java
@@ -12,13 +12,13 @@ import net.minecraft.server.v1_8_R3.MinecraftServer;
 import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
+import java.net.URL;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.jar.Manifest;
 
 public class CommandLag extends Command {
 
@@ -29,18 +29,40 @@ public class CommandLag extends Command {
 
     @Override
     public void execute(AuroraMCPlayer player, String aliasUsed, List<String> args) {
+        String buildNumber = null;
+        String gitCommit = null;
+        try {
+            Enumeration<URL> resources = getClass().getClassLoader()
+                    .getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                    Manifest manifest = new Manifest(resources.nextElement().openStream());
+                    // check that this is your manifest and do what you need or get the next one
+                if (manifest.getMainAttributes().containsKey("Module-Name")) {
+                    if (manifest.getMainAttributes().getValue("Module-Name").equals("AuroraMC-Core")) {
+                        buildNumber = manifest.getMainAttributes().getValue("Jenkins-Build-Number");
+                        gitCommit = manifest.getMainAttributes().getValue("Git-Commit");
+                    }
+                }
+            }
+        } catch (IOException e) {
+
+        }
+
+
         MemoryMXBean bean = ManagementFactory.getMemoryMXBean();
         long threadsCurrentlyExecuting = Bukkit.getScheduler().getActiveWorkers().size();
         long threadsAwaitingExecution = Bukkit.getScheduler().getPendingTasks().size();
         player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Resource Monitor", String.format("Server lag statistics:\n" +
                 "Used RAM: **%sMB**\n" +
                 "Total RAM: **%sMB**\n" +
+                "Current Build Number: **%s**\n" +
+                "Git Commit: **%s**" +
                 "Threads Executing: **%s**\n" +
                 "Threads Scheduled: **%s**\n" +
                 "TPS (1m, 5m, 15m): **%s**, **%s**, **%s**\n" +
                 "Stored values:\n" +
                 " - Players: **%s**\n" +
-                " - Open GUI's: **%s**", bean.getHeapMemoryUsage().getUsed()/1048576,Runtime.getRuntime().maxMemory()/1048576, threadsCurrentlyExecuting, threadsAwaitingExecution, new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[0]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[1]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[2]), AuroraMCAPI.getPlayers().size(), AuroraMCAPI.getOpenGUIs())));
+                " - Open GUI's: **%s**", bean.getHeapMemoryUsage().getUsed()/1048576,Runtime.getRuntime().maxMemory()/1048576, buildNumber, gitCommit, threadsCurrentlyExecuting, threadsAwaitingExecution, new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[0]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[1]),new DecimalFormat("##.##").format(MinecraftServer.getServer().recentTps[2]), AuroraMCAPI.getPlayers().size(), AuroraMCAPI.getOpenGUIs())));
     }
 
     @Override
