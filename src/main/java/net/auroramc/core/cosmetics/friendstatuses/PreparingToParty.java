baebd5a3a9635016462344ed0d2aed7fc79f6b37
BuildTools <unconfigured@null.spigotmc.org>
2021-12-04 19:48:18 +0000

    I hate formatting.
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/net/auroramc/core/cosmetics/friendstatuses/PreparingToParty.java
+++ src/main/java/net/auroramc/core/cosmetics/friendstatuses/PreparingToParty.java
@@ -11,6 +11,6 @@ import java.util.Collections;
 public class PreparingToParty extends FriendStatus {
 
     public PreparingToParty() {
-        super(124, "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", UnlockMode.CRATE, 0, Collections.emptyList(), Collections.emptyList(), "Purchase the Grand Celebration Bundle at;store.auroramc.net to unlock this status!", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", '4', true);
+        super(124, "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", UnlockMode.CRATE, 0, Collections.emptyList(), Collections.emptyList(), "Purchase the Grand Celebration Bundle at;&cstore.auroramc.net to unlock this status!", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", '4', true);
     }
 }
