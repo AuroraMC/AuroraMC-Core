614ecafb5be52af798f19603f1d6e66f92fed78c
BuildTools <unconfigured@null.spigotmc.org>
2021-12-04 19:04:38 +0000

    Add new friend status (Grand Celebration Bundle)
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ src/main/java/net/auroramc/core/cosmetics/friendstatuses/PreparingToParty.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2021 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.core.cosmetics.friendstatuses;
+
+import net.auroramc.core.api.cosmetics.FriendStatus;
+
+import java.util.Collections;
+
+public class PreparingToParty extends FriendStatus {
+
+    public PreparingToParty() {
+        super(124, "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", UnlockMode.CRATE, 0, Collections.emptyList(), Collections.emptyList(), "Purchase the Grand Celebration Bundle at store.auroramc.net to unlock this status!", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0", '4', true);
+    }
+}
