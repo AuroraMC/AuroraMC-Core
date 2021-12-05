2c4028df135e0b0096bb0630aad68adfe2a29de8
Heliology <burntcbusiness@gmail.com>
2021-12-05 04:41:00 +0000

    Update!
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/net/auroramc/core/cosmetics/friendstatuses/PreparingToParty.java
+++ src/main/java/net/auroramc/core/cosmetics/friendstatuses/PreparingToParty.java
@@ -11,6 +11,6 @@ import java.util.Collections;
 public class PreparingToParty extends FriendStatus {
 
     public PreparingToParty() {
-        super(124, "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0&r", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0&r", UnlockMode.CRATE, 0, Collections.emptyList(), Collections.emptyList(), "Purchase the Grand Celebration Bundle at;&cstore.auroramc.net to unlock this status!", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0&r", '4', true);
+        super(124, "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0&r", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0&r", UnlockMode.CRATE, 0, Collections.emptyList(), Collections.emptyList(), "Purchase the Grand Celebration Bundle at;&cstore.auroramc.net to unlock this friend status!", "&5&k0&r &4P&cr&6e&ep&aa&br&3i&9n&dg &4t&co &6p&ea&ar&bt&3y &5&k0&r", '4', true);
     }
 }
