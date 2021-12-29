a1db4866bb23203cf52c2fbade9b8a9f1487eac7
BuildTools <unconfigured@null.spigotmc.org>
2021-12-29 12:29:05 +0000

    More Cosmetic Changes
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/main/java/net/auroramc/core/cosmetics/plussymbols/Balanced.java
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/Balanced.java
@@ -12,6 +12,6 @@ import java.util.Collections;
 public class Balanced extends PlusSymbol {
 
     public Balanced() {
-        super(217, "Balanced", "&3&l☯ Balanced", "There is currently no description for this cosmetic!", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", true, Material.CAKE, (short)0, '☯');
+        super(217, "Balanced", "&3&l☯ Balanced", "", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", true, Material.CAKE, (short)0, '☯');
     }
 }
