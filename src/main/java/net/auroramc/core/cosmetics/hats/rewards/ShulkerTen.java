aa11178137d76eb7e2db9b09382f813eddb581ee
Heliology <burntcbusiness@gmail.com>
2021-12-30 11:21:18 +0000

    Cosmetic Testing
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerTen.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerTen.java
@@ -4,5 +4,13 @@
 
 package net.auroramc.core.cosmetics.hats.rewards;
 
-public class ShulkerTen {
+import net.auroramc.core.api.cosmetics.Hat;
+
+import java.util.Collections;
+
+public class ShulkerTen extends Hat {
+
+    public ShulkerTen() {
+        super(325, "Shulker Ten", "&3&lShulker Ten", "Use this to show off reaching Level 10!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 10!", "979f9a59c897af032d4002596fab2e4e9f22f5ae1ea370f755a5ee9396b372c9", true);
+    }
 }
