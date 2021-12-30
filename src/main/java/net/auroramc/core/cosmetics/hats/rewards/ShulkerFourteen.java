fda91f49b12d1440e025347f3a430b82dc1e41f1
Heliology <burntcbusiness@gmail.com>
2021-12-30 15:52:16 +0000

    Level Cosmetics Update (Yet to be done on core, don't push)
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerFourteen.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerFourteen.java
@@ -4,5 +4,13 @@
 
 package net.auroramc.core.cosmetics.hats.rewards;
 
-public class ShulkerFourteen {
+import net.auroramc.core.api.cosmetics.Hat;
+
+import java.util.Collections;
+
+public class ShulkerFourteen extends Hat {
+
+    public ShulkerFourteen() {
+        super(329, "Shulker Fourteen", "&3&lShulker Fourteen", "Use this to show off reaching Level 14!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 14!", "3155b63e94356ec3ae0e62eaf5e01faf332e30f629349742075a697baf55582a", true);
+    }
 }
--- src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerThirty.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerThirty.java
@@ -4,5 +4,13 @@
 
 package net.auroramc.core.cosmetics.hats.rewards;
 
-public class ShulkerThirty {
+import net.auroramc.core.api.cosmetics.Hat;
+
+import java.util.Collections;
+
+public class ShulkerThirty extends Hat {
+
+    public ShulkerThirty() {
+        super(345, "Shulker Thirty", "&3&lShulker Thirty", "Use this to show off reaching Level 30!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 30!", "f31417286a7fef4660b8b88226de9125871a41a2525fd38d64c57020df6232bd", true);
+    }
 }
