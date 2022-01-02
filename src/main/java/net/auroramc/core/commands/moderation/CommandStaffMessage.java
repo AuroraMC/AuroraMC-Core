61705fd078045877e168ae34260e15f8fbca0a82
Block2Block <accounts@block2block.me>
2022-01-02 18:05:19 +0000

    MS and RS are will now take into account disguise names
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/commands/moderation/CommandStaffMessage.java
+++ src/main/java/net/auroramc/core/commands/moderation/CommandStaffMessage.java
@@ -12,6 +12,7 @@ import net.auroramc.core.api.command.Command;
 import net.auroramc.core.api.permissions.Permission;
 import net.auroramc.core.api.permissions.Rank;
 import net.auroramc.core.api.players.AuroraMCPlayer;
+import net.auroramc.core.api.players.Disguise;
 import net.md_5.bungee.api.chat.BaseComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
@@ -31,6 +32,35 @@ public class CommandStaffMessage extends Command {
     public void execute(AuroraMCPlayer player, String aliasUsed, List<String> args) {
         if (args.size() > 1) {
             if (player.getActiveMutes().size() == 0) {
+                AuroraMCPlayer player1 = AuroraMCAPI.getDisguisedPlayer(args.get(0));
+                if (player1 != null) {
+                    if (player1.getActiveDisguise() != null) {
+                        player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Message", String.format("No match found for [**%s**]", args.get(0))));
+                        return;
+                    }
+                    args.remove(0);
+                    if (AuroraMCAPI.getFilter() == null) {
+                        player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Message", "Our chat filter is currently being updated. Please try again in a few seconds!"));
+                        return;
+                    }
+                    String message = AuroraMCAPI.getFilter().filter(String.join(" ", args));
+                    BaseComponent formatted = AuroraMCAPI.getFormatter().formatStaffMessage(player, player1, message);
+                    player1.getPlayer().spigot().sendMessage(AuroraMCAPI.getFormatter().formatStaffMessageFrom(player, message));
+                    player1.getPlayer().playSound(player1.getPlayer().getLocation(), Sound.NOTE_PLING, 100, 2);
+                    player.getPlayer().spigot().sendMessage(AuroraMCAPI.getFormatter().formatStaffMessageTo(player1, message));
+                    player.getPlayer().playSound(player.getPlayer().getLocation(), Sound.NOTE_PLING, 100, 2);
+                    player.setLastAdminMessage(player1.getPlayer().getUniqueId());
+                    for (Player p : Bukkit.getOnlinePlayers()) {
+                        if (p != player1.getPlayer() && p != player.getPlayer()) {
+                            if (AuroraMCAPI.getPlayer(p) != null) {
+                                if (AuroraMCAPI.getPlayer(p).hasPermission("moderation")) {
+                                    p.spigot().sendMessage(formatted);
+                                }
+                            }
+                        }
+                    }
+                    return;
+                }
                 Player proxiedPlayer = Bukkit.getPlayer(args.get(0));
                 if (proxiedPlayer != null) {
                     AuroraMCPlayer target = AuroraMCAPI.getPlayer(proxiedPlayer);
@@ -63,6 +93,42 @@ public class CommandStaffMessage extends Command {
                     new BukkitRunnable(){
                         @Override
                         public void run() {
+                            if (AuroraMCAPI.getDbManager().isAlreadyDisguise(args.get(0))) {
+                                UUID uuid = AuroraMCAPI.getDbManager().getUUIDFromDisguise(args.get(0));
+                                if (AuroraMCAPI.getDbManager().hasActiveSession(uuid)) {
+                                    args.remove(0);
+                                    if (AuroraMCAPI.getFilter() == null) {
+                                        player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Message", "Our chat filter is currently being updated. Please try again in a few seconds!"));
+                                        return;
+                                    }
+                                    String message = AuroraMCAPI.getFilter().filter(String.join(" ", args));
+
+                                    ProtocolMessage protocolMessage = new ProtocolMessage(Protocol.STAFF_MESSAGE, AuroraMCAPI.getDbManager().getServer(uuid), uuid.toString(), player.getPlayer().getUniqueId().toString(), message);
+                                    CommunicationUtils.sendMessage(protocolMessage);
+
+                                    String name = AuroraMCAPI.getDbManager().getNameFromUUID(uuid.toString());
+                                    Rank rank = AuroraMCAPI.getDbManager().getRank(uuid);
+                                    Disguise activeDisguise = AuroraMCAPI.getDbManager().getDisguise(uuid.toString());
+                                    if (activeDisguise != null) {
+                                        rank = activeDisguise.getRank();
+                                        name = activeDisguise.getName();
+                                    }
+
+                                    BaseComponent formatted = AuroraMCAPI.getFormatter().formatStaffMessage(player, rank, name, message);
+                                    player.getPlayer().spigot().sendMessage(AuroraMCAPI.getFormatter().formatStaffMessageTo(rank, name, message));
+                                    player.getPlayer().playSound(player.getPlayer().getLocation(), Sound.NOTE_PLING, 100, 2);
+
+                                    for (Player p : Bukkit.getOnlinePlayers()) {
+                                        if (p != player.getPlayer()) {
+                                            if (AuroraMCAPI.getPlayer(p) != null) {
+                                                if (AuroraMCAPI.getPlayer(p).hasPermission("moderation")) {
+                                                    p.spigot().sendMessage(formatted);
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
                             UUID uuid = AuroraMCAPI.getDbManager().getUUID(args.get(0));
                             if (uuid != null) {
                                 if (AuroraMCAPI.getDbManager().hasActiveSession(uuid)) {
@@ -79,6 +145,11 @@ public class CommandStaffMessage extends Command {
 
                                     String name = AuroraMCAPI.getDbManager().getNameFromUUID(uuid.toString());
                                     Rank rank = AuroraMCAPI.getDbManager().getRank(uuid);
+                                    Disguise activeDisguise = AuroraMCAPI.getDbManager().getDisguise(uuid.toString());
+                                    if (activeDisguise != null) {
+                                        rank = activeDisguise.getRank();
+                                        name = activeDisguise.getName();
+                                    }
 
                                     BaseComponent formatted = AuroraMCAPI.getFormatter().formatStaffMessage(player, rank, name, message);
                                     player.getPlayer().spigot().sendMessage(AuroraMCAPI.getFormatter().formatStaffMessageTo(rank, name, message));
