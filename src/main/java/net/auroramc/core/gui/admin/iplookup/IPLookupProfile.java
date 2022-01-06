6b08a841fdd26f0b6255c2593e26768d38ff5edd
Block2Block <accounts@block2block.me>
2022-01-06 14:13:29 +0000

    Fixed some visual bugs
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/gui/admin/iplookup/IPLookupProfile.java
+++ src/main/java/net/auroramc/core/gui/admin/iplookup/IPLookupProfile.java
@@ -19,12 +19,12 @@ public class IPLookupProfile extends GUI {
 
     public IPLookupProfile(IPProfile playerProfile) {
         super("&3&l IP Lookup for #" + playerProfile.getId(), 2, true);
-        border("&3&l IP Lookup for profile #" + playerProfile.getId(), "");
+        border("&3&l IP Lookup for Profile #" + playerProfile.getId(), "");
 
-        this.setItem(0, 4, new GUIItem(Material.PAPER, String.format("&3&lIP Lookup for profile# %s", playerProfile.getId()), 1, "", (short)3, false));
+        this.setItem(0, 4, new GUIItem(Material.PAPER, String.format("&3&lIP Lookup for Profile #%s", playerProfile.getId())));
 
         this.setItem(1, 1, new GUIItem(Material.EMERALD_BLOCK, "&3&lPunishment Information", 1, String.format(";&rOf known accounts:;**%s** are currently muted.;**%s** are currently banned.", playerProfile.getMutes(), playerProfile.getBans())));
-        this.setItem(1, 3, new GUIItem(Material.BOOK, "&3&lAccounts", 1, String.format(";&rThis IP Profile is;&rshared with **%s** other accounts.;;Most recent alts:;**%s**", playerProfile.getNumberOfAccounts(), String.join("**;**", playerProfile.getAccounts()))));
+        this.setItem(1, 3, new GUIItem(Material.BOOK, "&3&lAccounts", 1, String.format(";&rThis IP Profile is;&rshared with **%s** other accounts.;;&rMost recent alts:;**%s**", playerProfile.getNumberOfAccounts(), String.join("**;**", playerProfile.getAccounts()))));
         this.setItem(1, 5, new GUIItem(Material.BARRIER, "&3&lLatest Information", 1, String.format("&rThis IP was last used by:;**%s**;;&rDate:;**%s**", playerProfile.getLastUsedBy(), new Date(playerProfile.getLastUsedAt()))));
         this.setItem(1, 7, new GUIItem(Material.REDSTONE_BLOCK, "&3&lGlobal Account Suspension Status", 1, String.format(";&rStatus: %s%s", ((playerProfile.isGlobalAccountSuspended())?"&cSuspended":"&aNot Suspended"), ((playerProfile.isGlobalAccountSuspended())?";&rReason: **" + playerProfile.getGlobalAccountSuspensionReason() + "**":""))));
     }
