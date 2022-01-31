df966c0ff46b7c67c65f772d95c976279a0cc0fe
Block2Block <accounts@block2block.me>
2022-01-31 18:09:27 +0000

    Add build versions command
    Signed-off-by: Block2Block <accounts@block2block.me>

---
+++ src/main/java/net/auroramc/core/commands/admin/CommandBuildVersions.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2021-2021 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.core.commands.admin;
+
+import net.auroramc.core.api.AuroraMCAPI;
+import net.auroramc.core.api.command.Command;
+import net.auroramc.core.api.permissions.Permission;
+import net.auroramc.core.api.players.AuroraMCPlayer;
+import net.minecraft.server.v1_8_R3.MinecraftServer;
+import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.util.*;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+public class CommandBuildVersions extends Command {
+
+
+    public CommandBuildVersions() {
+        super("bversions", Collections.singletonList("buildversions"), Arrays.asList(Permission.ALL, Permission.DEBUG_INFO), false, null);
+    }
+
+    @Override
+    public void execute(AuroraMCPlayer player, String aliasUsed, List<String> args) {
+        StringBuilder sb = new StringBuilder();
+        String buildNumber = null;
+        String gitCommit = null;
+        try {
+            Enumeration<URL> resources = AuroraMCAPI.getCore().getClass().getClassLoader()
+                    .getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements()) {
+                    Manifest manifest = new Manifest(resources.nextElement().openStream());
+                    // check that this is your manifest and do what you need or get the next one
+                    Attributes attributes = manifest.getMainAttributes();
+
+                buildNumber = attributes.getValue("Jenkins-Build-Number");
+                gitCommit = attributes.getValue("Git-Commit");
+                if (buildNumber != null && gitCommit != null) {
+                    sb.append("\n**");
+                    sb.append(attributes.getValue("Module-Name"));
+                    sb.append("**:\n" +
+                            "Build Number: **");
+                    sb.append(buildNumber);
+                    sb.append("**\n" +
+                            "Git Commit: **");
+                    sb.append(gitCommit);
+                    sb.append("**");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        player.getPlayer().sendMessage(AuroraMCAPI.getFormatter().pluginMessage("Server Manager", "Current Server Build Numbers:" +
+                sb));
+    }
+
+    @Override
+    public @NotNull List<String> onTabComplete(AuroraMCPlayer player, String aliasUsed, List<String> args, String lastToken, int numberArguments) {
+        return new ArrayList<>();
+    }
+}
