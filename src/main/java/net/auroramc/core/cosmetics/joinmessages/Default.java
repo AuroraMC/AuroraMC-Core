62b0426bc9d578ef93391d3711568dbfcd3fba4d
Heliology <burntcbusiness@gmail.com>
2022-02-06 18:38:52 +0000

    Create Default Join Message
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

---
+++ src/main/java/net/auroramc/core/cosmetics/joinmessages/Default.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2022 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.core.cosmetics.joinmessages;
+
+import net.auroramc.core.api.cosmetics.JoinMessage;
+import net.auroramc.core.api.permissions.Permission;
+import net.auroramc.core.api.permissions.Rank;
+import net.auroramc.core.api.players.AuroraMCPlayer;
+import org.bukkit.Material;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Default extends JoinMessage {
+
+    public Default() {
+        super(400, "&3&lDefault", "&3&lDefault", "The default join message.", UnlockMode.ALL, -1, Collections.emptyList(), Collections.emptyList(), "", true, Material.NAME_TAG, (short)0);
+    }
+
+    @Override
+    protected String onJoin(AuroraMCPlayer player) {
+        return String.format("%s has joined the game.", player.getPlayer().getName());
+    }
+}
