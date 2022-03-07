589c78e853019aa2628104b7361919a61ded9687
Heliology <burntcbusiness@gmail.com>
2022-03-07 22:39:19 +0000

    Fix Rainbow Kill Messages
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

--- src/main/java/net/auroramc/core/cosmetics/killmessages/Rainbow.java
+++ src/main/java/net/auroramc/core/cosmetics/killmessages/Rainbow.java
@@ -27,69 +27,69 @@ public class Rainbow extends KillMessage {
         switch (reason) {
             case MELEE: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was killed by %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was killed by %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was killed."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was killed.", victim.getPlayer().getName()));
                 }
             }
             case BOW: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was shot by %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was shot by %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was shot."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was shot.", victim.getPlayer().getName()));
                 }
             }
             case VOID: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was thrown into the void by %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was thrown into the void by %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s fell into the void."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s fell into the void.", victim.getPlayer().getName()));
                 }
             }
             case FALL: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was thrown off a cliff by %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was thrown off a cliff by %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was thrown off a cliff."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was thrown off a cliff.", victim.getPlayer().getName()));
                 }
 
             }
             case TNT: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was blown up by %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was blown up by %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s blew up"), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s blew up", victim.getPlayer().getName()));
                 }
             }
             case ENTITY: {
-                return String.format(AuroraMCAPI.getFormatter().rainbow("**%s** was killed by **%s**."), victim.getPlayer().getName(), WordUtils.capitalizeFully(WordUtils.capitalizeFully(entity.getType().name().replace("_", " "))));
+                return AuroraMCAPI.getFormatter().rainbow(String.format("**%s** was killed by **%s**.", victim.getPlayer().getName(), WordUtils.capitalizeFully(WordUtils.capitalizeFully(entity.getType().name().replace("_", " ")))));
             }
             case DROWNING: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s drowned trying to escape %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s drowned trying to escape %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s drowned."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s drowned.", victim.getPlayer().getName()));
                 }
             }
             case LAVA: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s fell in lava trying to escape %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s fell in lava trying to escape %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s fell in lava."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s fell in lava.", victim.getPlayer().getName()));
                 }
             }
             case FIRE: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s burnt to death trying to escape %s"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s burnt to death trying to escape %s", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s burnt to death."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s burnt to death.", victim.getPlayer().getName()));
                 }
             }
             case UNKNOWN: {
                 if (killer != null) {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was killed by %s using magic"), victim.getPlayer().getName(), killer.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was killed by %s using magic", victim.getPlayer().getName(), killer.getPlayer().getName()));
                 } else {
-                    return String.format(AuroraMCAPI.getFormatter().rainbow("%s was killed by magic."), victim.getPlayer().getName());
+                    return AuroraMCAPI.getFormatter().rainbow(String.format("%s was killed by magic.", victim.getPlayer().getName()));
                 }
             }
         }
