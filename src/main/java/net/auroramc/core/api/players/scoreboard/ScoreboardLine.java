e1337dbad8598a3930cae77eeb9a5c73cf41cefa
Block2Block <accounts@block2block.me>
2022-03-09 22:40:49 +0000

    Undo previous change
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/api/players/scoreboard/ScoreboardLine.java
+++ src/main/java/net/auroramc/core/api/players/scoreboard/ScoreboardLine.java
@@ -4,7 +4,6 @@
 
 package net.auroramc.core.api.players.scoreboard;
 
-import net.auroramc.core.api.AuroraMCAPI;
 import org.bukkit.ChatColor;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
@@ -12,22 +11,21 @@ import org.bukkit.scoreboard.Team;
 
 public class ScoreboardLine {
 
+    private static final char[] colours = {'0','1','2','3','4','5','6','7','8','9', 'a', 'b', 'c', 'd', 'e', 'f'};
 
     private final Objective objective;
     private final int line;
     private String text;
     private final Team team;
-    private final String user;
 
     public ScoreboardLine(Scoreboard scoreboard, Objective objective, int line, String text) {
         this.line = line;
         this.team = scoreboard.registerNewTeam("line" + line);
-        this.user = new String(new char[line]).replace("\0", "֍");
-        team.addEntry(user);
+        team.addEntry("§" + colours[line]);
         this.text = text;
         if (this.text.length() > 16) {
             team.setPrefix(this.text.substring(0, 16));
-            String suffix = this.text.substring(16);
+            String suffix = ChatColor.getLastColors(team.getPrefix()) + this.text.substring(16);
             if (suffix.length() > 16) {
                 suffix = suffix.substring(0, 16);
             }
@@ -39,18 +37,18 @@ public class ScoreboardLine {
     }
 
     public void apply() {
-        objective.getScore(user).setScore(line);
+        objective.getScore("§" + colours[line]).setScore(line);
     }
 
     public void remove() {
-        objective.getScoreboard().resetScores(user);
+        objective.getScoreboard().resetScores("§" + colours[line]);
     }
 
     public void setText(String text) {
         this.text = text;
         if (this.text.length() > 16) {
             team.setPrefix(this.text.substring(0, 16));
-            String suffix = this.text.substring(16);
+            String suffix = ChatColor.getLastColors(team.getPrefix()) + this.text.substring(16);
             if (suffix.length() > 16) {
                 suffix = suffix.substring(0, 16);
             }
