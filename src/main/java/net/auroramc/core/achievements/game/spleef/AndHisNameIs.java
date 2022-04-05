183ce95420467a9e476f93aa9da0b4121da262ea
Heliology <burntcbusiness@gmail.com>
2022-04-05 18:47:24 +0100

    Add Spleef Achievements
    Signed-off-by: Heliology <burntcbusiness@gmail.com>

---
+++ src/main/java/net/auroramc/core/achievements/game/spleef/AndHisNameIs.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.core.achievements.game.spleef;
+
+import net.auroramc.core.api.stats.Achievement;
+
+public class AndHisNameIs extends Achievement {
+    public AndHisNameIs() {
+        super(124, "And his name is...", "Punch someone out of the ring", "None", true, false, AchievementCategory.GAME, 100);
+    }
+}
