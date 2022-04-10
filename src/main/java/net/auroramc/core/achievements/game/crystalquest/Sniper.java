4ee6c6bbf2d53943a6fb0ace64deb81cacdbc2a8
Block2Block <accounts@block2block.me>
2022-04-10 20:20:26 +0100

    Fix achievements
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/achievements/game/crystalquest/Sniper.java
+++ src/main/java/net/auroramc/core/achievements/game/crystalquest/Sniper.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class Sniper extends Achievement {
     public Sniper() {
-        super(71, "Sniper", "Hit a player with an arrow from 50 blocks away.", "None", true, false, AchievementCategory.GAME, 1);
+        super(71, "Sniper", "Kill a player with an arrow from 50 blocks away.", "None", true, false, AchievementCategory.GAME, 1);
     }
 }
