f96a279e9b1ca848d640878ee61695f086a69ad2
Heliology <burntcbusiness@gmail.com>
2022-04-19 13:33:45 +0100

    Add two new plus symbols (add to proxy later)

---
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/kitrewards/Sword.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2022 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.core.cosmetics.plussymbols.kitrewards;
+
+import net.auroramc.core.api.cosmetics.PlusSymbol;
+import net.auroramc.core.api.permissions.Permission;
+import net.auroramc.core.api.permissions.Rank;
+import org.bukkit.Material;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Sword extends PlusSymbol {
+    public Sword() {
+        super(222, "Sword", "&6&l⚔ Sword", "", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Reach Crystal Quest Fighter Kit Prestige 1 to unlock this Plus Symbol!", true, Material.DIAMOND_SWORD, (short)0, '⚔', Rarity.MYTHICAL);
+    }
+}
