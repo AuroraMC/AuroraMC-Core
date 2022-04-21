ae7adc11cf1bbdf29f014a88b317b4c7a5c0f1ae
Block2Block <accounts@block2block.me>
2022-04-21 22:28:23 +0300

    Server messages now take into account recipient
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/cosmetics/servermessages/Murderer.java
+++ src/main/java/net/auroramc/core/cosmetics/servermessages/Murderer.java
@@ -17,12 +17,24 @@ public class Murderer extends ServerMessage {
     }
 
     @Override
-    public String onJoin(AuroraMCPlayer player) {
-        return String.format("**%s** wants to &c&lKILL!", ((player.isDisguised())?player.getActiveDisguise().getName():player.getName()));
+    public String onJoin(AuroraMCPlayer recipient, AuroraMCPlayer player) {
+        String name = player.getPlayer().getName();
+        if (recipient.equals(player)) {
+            if (recipient.isDisguised() && recipient.getPreferences().isHideDisguiseNameEnabled()) {
+                name = player.getName();
+            }
+        }
+        return String.format("**%s** wants to &c&lKILL!", name);
     }
 
     @Override
-    public String onLeave(AuroraMCPlayer player) {
-        return String.format("**%s** has finished their &c&lRAMPAGE.", player.getPlayer().getName());
+    public String onLeave(AuroraMCPlayer recipient, AuroraMCPlayer player) {
+        String name = player.getPlayer().getName();
+        if (recipient.equals(player)) {
+            if (recipient.isDisguised() && recipient.getPreferences().isHideDisguiseNameEnabled()) {
+                name = player.getName();
+            }
+        }
+        return String.format("**%s** has finished their &c&lRAMPAGE.", name);
     }
 }
