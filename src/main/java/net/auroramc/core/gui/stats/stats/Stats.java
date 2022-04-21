fe83cf3a984b32aa6af2b96bec8cb5dfa131bd12
Block2Block <accounts@block2block.me>
2022-04-21 23:40:09 +0300

    Implement paintball statistics
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/gui/stats/stats/Stats.java
+++ src/main/java/net/auroramc/core/gui/stats/stats/Stats.java
@@ -115,6 +115,13 @@ public class Stats extends GUI {
                 AuroraMCAPI.openGUI(player, stats);
                 break;
             }
+            case SNOW_BALL: {
+                AuroraMCAPI.closeGUI(player);
+                PaintballStatistics stats = new PaintballStatistics(player, name, this.stats, this.subscription, playerId);
+                stats.open(player);
+                AuroraMCAPI.openGUI(player, stats);
+                break;
+            }
             default: {
                 player.getPlayer().playSound(player.getPlayer().getLocation(), Sound.ITEM_BREAK, 100, 0);
             }
