092465e5b2ed589a89049834aebd1d8af4b87452
Heliology <burntcbusiness@gmail.com>
2022-06-01 16:15:44 +0100

    Update Rarities

--- src/main/java/net/auroramc/core/cosmetics/plussymbols/Plus.java
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/Plus.java
@@ -13,6 +13,6 @@ import java.util.Collections;
 public class Plus extends PlusSymbol {
 
     public Plus() {
-        super(206, "Plus", "&3&l✚ Plus", "Show off your Plus Subscription with this Plus Symbol!", UnlockMode.PERMISSION, -1, Collections.singletonList(Permission.PLUS), Collections.emptyList(), "Subscribe to Plus at store.auroramc.net to unlock this symbol!", true, Material.COOKIE, (short)0, '✚', Rarity.COMMON);
+        super(206, "Plus", "&3&l✚ Plus", "Show off your Plus Subscription with this Plus Symbol!", UnlockMode.PERMISSION, -1, Collections.singletonList(Permission.PLUS), Collections.emptyList(), "Subscribe to Plus at store.auroramc.net to unlock this symbol!", true, Material.COOKIE, (short)0, '✚', Rarity.UNCOMMON);
     }
 }
