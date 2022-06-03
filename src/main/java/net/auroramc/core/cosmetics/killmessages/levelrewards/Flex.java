7d82508361fee2c390520627613e7c1fa46b8cd5
Block2Block <accounts@block2block.me>
2022-06-03 17:42:31 +0100

    Remove +1
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/cosmetics/killmessages/levelrewards/Flex.java
+++ src/main/java/net/auroramc/core/cosmetics/killmessages/levelrewards/Flex.java
@@ -38,83 +38,83 @@ public class Flex extends KillMessage {
         switch (reason) {
             case MELEE: {
                 if (killer != null) {
-                    return String.format("**%s** was kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has died **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has died **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
             case BOW: {
                 if (killer != null) {
-                    return String.format("**%s** was archery kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was archery kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has been shot **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths" + reason.name()) + 1);
+                    return String.format("**%s** has been shot **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths" + reason.name()));
                 }
             }
             case VOID: {
                 if (killer != null) {
-                    return String.format("**%s** was void kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was void kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has fallen into the void **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has fallen into the void **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
             case FALL: {
                 if (killer != null) {
-                    return String.format("**%s** was cliff kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was cliff kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has fallen off a cliff **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has fallen off a cliff **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
 
             }
             case TNT: {
                 if (killer != null) {
-                    return String.format("**%s** was TNT kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was TNT kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has blown up **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has blown up **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
             case ENTITY: {
-                return String.format("**%s** has died **#%s** times to entities, this time at the hands of **%s**.", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1, WordUtils.capitalizeFully(WordUtils.capitalizeFully(entity.getType().name().replace("_", " "))));
+                return String.format("**%s** has died **#%s** times to entities, this time at the hands of **%s**.", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()), WordUtils.capitalizeFully(WordUtils.capitalizeFully(entity.getType().name().replace("_", " "))));
             }
             case DROWNING: {
                 if (killer != null) {
-                    return String.format("**%s** was drown kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was drown kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has drowned **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has drowned **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
             case LAVA: {
                 if (killer != null) {
-                    return String.format("**%s** was lava kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was lava kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has died to lava **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has died to lava **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
             case FIRE: {
                 if (killer != null) {
-                    return String.format("**%s** was fire kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was fire kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has died to fire **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has died to fire **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
             case PAINTBALL: {
                 if (entity != null) {
-                    return String.format("**%s** was paintball kill **#%s** for **%s**'s Turret", victimName, killer.getStats().getStatistic(3, "kills.turret") + 1 , killerName);
+                    return String.format("**%s** was paintball kill **#%s** for **%s**'s Turret", victimName, killer.getStats().getStatistic(3, "kills.turret") , killerName);
                 } else {
                     if (killer != null) {
-                        return String.format("**%s** was paintball kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(3, "kills") + 1 , killerName);
+                        return String.format("**%s** was paintball kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(3, "kills") , killerName);
                     } else {
-                        return String.format("**%s** has been magically died to a paintball **%s** times", victimName, victim.getStats().getStatistic(3, "deaths") + 1);
+                        return String.format("**%s** has been magically died to a paintball **%s** times", victimName, victim.getStats().getStatistic(3, "deaths"));
                     }
                 }
             }
             case TAG: {
-                return String.format("**%s** was tag **#%s** for **%s**", victimName, killer.getStats().getStatistic(103, "tags") + 1, killerName);
+                return String.format("**%s** was tag **#%s** for **%s**", victimName, killer.getStats().getStatistic(103, "tags"), killerName);
             }
             case UNKNOWN: {
                 if (killer != null) {
-                    return String.format("**%s** was magic kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()) + 1, killerName);
+                    return String.format("**%s** was magic kill **#%s** for **%s**", victimName, killer.getStats().getStatistic(gameId, "kills." + reason.name()), killerName);
                 } else {
-                    return String.format("**%s** has magically died **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()) + 1);
+                    return String.format("**%s** has magically died **#%s** times", victimName, victim.getStats().getStatistic(gameId, "deaths." + reason.name()));
                 }
             }
         }
