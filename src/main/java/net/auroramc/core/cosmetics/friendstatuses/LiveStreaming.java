fc2fc1bf9aff3be9ee4525807c8f698eb528dd3f
Heliology <burntcbusiness@gmail.com>
2022-06-03 17:30:18 +0100

    Update Rarities of Friend Statuses and Plus Symbols

--- src/main/java/net/auroramc/core/cosmetics/friendstatuses/LiveStreaming.java
+++ src/main/java/net/auroramc/core/cosmetics/friendstatuses/LiveStreaming.java
@@ -12,6 +12,6 @@ import java.util.Collections;
 public class LiveStreaming extends FriendStatus {
 
     public LiveStreaming() {
-        super(108, "Live Streaming", "Live Streaming", UnlockMode.PERMISSION, -1, Collections.singletonList(Permission.SOCIAL), Collections.emptyList(), "", "Live Streaming", '5', false, Rarity.COMMON);
+        super(108, "Live Streaming", "Live Streaming", UnlockMode.PERMISSION, -1, Collections.singletonList(Permission.SOCIAL), Collections.emptyList(), "", "Live Streaming", '5', false, Rarity.MYTHICAL);
     }
 }
