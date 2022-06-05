a54fed0fb5fa6918a00ec82fa4ee7a0979ddd77e
Heliology <burntcbusiness@gmail.com>
2022-06-06 00:26:55 +0100

    block smells

--- src/main/java/net/auroramc/core/cosmetics/plussymbols/Peace.java
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/Peace.java
@@ -12,6 +12,6 @@ import java.util.Collections;
 public class Peace extends PlusSymbol {
 
     public Peace() {
-        super(209, "Peace", "&3&l✌ Peace", "It's all cool brooo", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the AuroraMC Starter Pack at;store.auroramc.net to unlock this plus symbol!", true, Material.SLIME_BALL, (short)0, '✌', Rarity.EPIC);
+        super(209, "Peace", "&3&l✌ Peace", "It's all cool brooo", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the AuroraMC Starter Pack at store.auroramc.net to unlock this plus symbol!", true, Material.SLIME_BALL, (short)0, '✌', Rarity.EPIC);
     }
 }
