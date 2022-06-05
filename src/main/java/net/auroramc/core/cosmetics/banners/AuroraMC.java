a99e6087237e12da96e400a735a8fd04f082a464
Heliology <burntcbusiness@gmail.com>
2022-06-05 23:31:21 +0100

    Update rarity and unlock mode of AuroraMC banner

--- src/main/java/net/auroramc/core/cosmetics/banners/AuroraMC.java
+++ src/main/java/net/auroramc/core/cosmetics/banners/AuroraMC.java
@@ -26,6 +26,6 @@ public class AuroraMC extends Banner {
     }
 
     public AuroraMC() {
-        super(8, "AuroraMC?", "&3&lAuroraMC?", "&3It's the best we could do, okay?", UnlockMode.CRATE, 100, new ArrayList<>(), new ArrayList<>(), "Found in Crates", patterns, DyeColor.WHITE, true, Rarity.COMMON);
+        super(8, "AuroraMC?", "&3&lAuroraMC?", "&3It's the best we could do, okay?", UnlockMode.STORE_PURCHASE, 100, new ArrayList<>(), new ArrayList<>(), "Purchase the AuroraMC Starter Pack at;&cstore.auroramc.net to unlock this banner!", patterns, DyeColor.WHITE, true, Rarity.EPIC);
     }
 }
