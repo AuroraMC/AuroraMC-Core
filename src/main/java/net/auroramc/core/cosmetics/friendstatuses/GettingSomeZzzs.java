190c86fa0c836880d179a88ecf0c89e88bcbea9c
Heliology <burntcbusiness@gmail.com>
2022-06-08 17:17:22 +0100

    Update cosmetic rarities

--- src/main/java/net/auroramc/core/cosmetics/friendstatuses/GettingSomeZzzs.java
+++ src/main/java/net/auroramc/core/cosmetics/friendstatuses/GettingSomeZzzs.java
@@ -11,6 +11,6 @@ import java.util.Collections;
 public class GettingSomeZzzs extends FriendStatus {
 
     public GettingSomeZzzs() {
-        super(117, "&9Getting some Zzzs", "&9Getting some Zzzs", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", "&9&lGetting some Zzzs", '9', true, Rarity.COMMON);
+        super(117, "&9Getting some Zzzs", "&9Getting some Zzzs", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", "&9&lGetting some Zzzs", '9', true, Rarity.UNCOMMON);
     }
 }
