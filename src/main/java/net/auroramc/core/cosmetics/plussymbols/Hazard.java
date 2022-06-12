8efce811a9a0e0d9c18ae604187672f394bdcf1c
Heliology <burntcbusiness@gmail.com>
2022-06-12 18:26:47 +0100

    Update cosmetic rarities, update display colour of common rarity

--- src/main/java/net/auroramc/core/cosmetics/plussymbols/Hazard.java
+++ src/main/java/net/auroramc/core/cosmetics/plussymbols/Hazard.java
@@ -12,6 +12,6 @@ import java.util.Collections;
 public class Hazard extends PlusSymbol {
 
     public Hazard() {
-        super(220, "Hazard", "&3&l☣ Hazard", "WARNING! WARNING!", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", true, Material.BARRIER, (short)0, '☣', Rarity.COMMON);
+        super(220, "Hazard", "&3&l☣ Hazard", "WARNING! WARNING!", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", true, Material.BARRIER, (short)0, '☣', Rarity.UNCOMMON);
     }
 }
