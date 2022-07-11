a4f3fc8d5122ebda2ecb70a5674dfd30d1efd968
Heliology <burntcbusiness@gmail.com>
2022-07-11 17:23:17 +0100

    Update friend statuses

--- src/main/java/net/auroramc/core/cosmetics/friendstatuses/EatingDinner.java
+++ src/main/java/net/auroramc/core/cosmetics/friendstatuses/EatingDinner.java
@@ -11,6 +11,6 @@ import java.util.Collections;
 public class EatingDinner extends FriendStatus {
 
     public EatingDinner() {
-        super(121, "&bEating Dinner", "&bEating Dinner", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the AuroraMC Starter Pack at;&cstore.auroramc.net to unlock this friend status!", "&b&lEating Dinner", 'b', true, Rarity.EPIC);
+        super(121, "&bEating Dinner", "&bEating Dinner", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the AuroraMC Starter Pack at;&cstore.auroramc.net to unlock this friend status!", "&b&lEating Dinner", 'b', true, Rarity.EPIC);
     }
 }
