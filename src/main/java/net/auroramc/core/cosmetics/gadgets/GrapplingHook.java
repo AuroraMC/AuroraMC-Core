7b2073cb8a85ca7539c6b3bb40e62cadd5a1d360
Block2Block <accounts@block2block.me>
2022-07-27 17:38:02 +0100

    Implement Grappling Hook
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/cosmetics/gadgets/GrapplingHook.java
+++ src/main/java/net/auroramc/core/cosmetics/gadgets/GrapplingHook.java
@@ -4,22 +4,29 @@
 
 package net.auroramc.core.cosmetics.gadgets;
 
+import net.auroramc.core.api.AuroraMCAPI;
 import net.auroramc.core.api.cosmetics.Gadget;
 import net.auroramc.core.api.permissions.Permission;
 import net.auroramc.core.api.permissions.Rank;
 import net.auroramc.core.api.players.AuroraMCPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.util.Vector;
 
 import java.util.Collections;
 import java.util.List;
 
-public class GrapplingHook extends Gadget {
+public class GrapplingHook extends Gadget implements Listener {
 
     /*private static*/
 
     public GrapplingHook() {
-        super(801, "Grappling Hook", "&c&lGrappling Hook", "Spiderman, Spiderman, does whatever a Spiderman does.", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "You are now a chicken. You will lay an egg when you die.", true, Material.FISHING_ROD, (short)0, Rarity.EPIC, 10);
+        super(801, "Grappling Hook", "&c&lGrappling Hook", "Spiderman, Spiderman, does whatever a Spiderman does.", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the AuroraMC Starter Pack at store.auroramc.net to unlock this Gadget!", true, Material.FISHING_ROD, (short)0, Rarity.EPIC, 10);
+        Bukkit.getPluginManager().registerEvents(this, AuroraMCAPI.getCore());
     }
 
     @Override
@@ -28,10 +35,15 @@ public class GrapplingHook extends Gadget {
 
     @Override
     public void onUse(AuroraMCPlayer player, Location location) {
-        /*player.getPlayer().
-        if (player.) {
+    }
 
-        }*/
+    @EventHandler
+    public void onFish(PlayerFishEvent e) {
+        AuroraMCPlayer player = AuroraMCAPI.getPlayer(e.getPlayer());
+        if (this.equals(player.getActiveCosmetics().get(CosmeticType.GADGET))) {
+            Vector vector = e.getHook().getLocation().toVector().subtract(player.getPlayer().getLocation().toVector()).normalize();
+            player.getPlayer().setVelocity(vector);
+        }
     }
 
 }
