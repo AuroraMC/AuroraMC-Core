d13e82b94dc5acdb3ce902f41eaa3137530544cf
Heliology <burntcbusiness@gmail.com>
2022-10-28 15:29:07 +0100

    Hide hats given as level rewards if not unlocked

--- src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerEight.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerEight.java
@@ -11,6 +11,6 @@ import java.util.Collections;
 public class ShulkerEight extends Hat {
 
     public ShulkerEight() {
-        super(323, "Shulker Eight", "&3&lShulker Eight", "Use this to show off reaching Level 8!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 8!", "817aa92d242334443ed49f870ab56fbe542322ea0473a91a33a6f8302d55bf5e", true, Rarity.COMMON);
+        super(323, "Shulker Eight", "&3&lShulker Eight", "Use this to show off reaching Level 8!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 8!", "817aa92d242334443ed49f870ab56fbe542322ea0473a91a33a6f8302d55bf5e", false, Rarity.COMMON);
     }
 }
--- src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerTwentyFour.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/rewards/ShulkerTwentyFour.java
@@ -11,6 +11,6 @@ import java.util.Collections;
 public class ShulkerTwentyFour extends Hat {
 
     public ShulkerTwentyFour() {
-        super(339, "Shulker Twenty Four", "&3&lShulker Twenty Four", "Use this to show off reaching Level 24!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 24!", "815d8ceb70b088b258514d6f4e2c160edd3b356a5b0811907af8aa7da518fdc2", true, Rarity.COMMON);
+        super(339, "Shulker Twenty Four", "&3&lShulker Twenty Four", "Use this to show off reaching Level 24!", UnlockMode.LEVEL, -1, Collections.emptyList(), Collections.emptyList(), "Unlocked by reaching AuroraMC Level 24!", "815d8ceb70b088b258514d6f4e2c160edd3b356a5b0811907af8aa7da518fdc2", false, Rarity.COMMON);
     }
 }
