b8ffb946a14fbddf42cfd531705b5660c059164b
Heliology <burntcbusiness@gmail.com>
2022-10-29 22:53:19 +0100

    Update Is that all you've got achievement

--- src/main/java/net/auroramc/core/achievements/lobby/IsThatAllYouveGot.java
+++ src/main/java/net/auroramc/core/achievements/lobby/IsThatAllYouveGot.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class IsThatAllYouveGot extends Achievement {
     public IsThatAllYouveGot() {
-        super(206, "Is that all you've got?", "Achieve a time of less than 4 minutes on the Hard Parkour!", "None", true, true, AchievementCategory.LOBBY);
+        super(206, "Is that all you've got?", "Achieve a time of less than 5 minutes 30 seconds on the Hard Parkour!", "None", true, true, AchievementCategory.LOBBY);
     }
 }
