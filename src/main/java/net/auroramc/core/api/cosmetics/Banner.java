e6a7397c24c5ab17f0e4a3aa5837c042a8c38ff2
Block2Block <accounts@block2block.me>
2022-12-09 15:19:19 +0000

    Fix formatting issue with hat/banner item lore
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/api/cosmetics/Banner.java
+++ src/main/java/net/auroramc/core/api/cosmetics/Banner.java
@@ -69,8 +69,8 @@ public abstract class Banner extends Cosmetic {
 
         List<String> lore = new ArrayList<>();
         lore.add("");
-        lore.addAll(Arrays.asList(AuroraMCAPI.getFormatter().convert(("&r" + WordUtils.wrap(getDescription(), 40, ";&r", false))).split(";")));
-        lore.add(AuroraMCAPI.getFormatter().convert("&rRarity: " + this.getRarity().getDisplayName()));
+        lore.addAll(Arrays.asList(AuroraMCAPI.getFormatter().convert("&r&f" + WordUtils.wrap(getDescription(), 40, ";&r&f", false)).split(";")));
+        lore.add(AuroraMCAPI.getFormatter().convert("&r&fRarity: " + this.getRarity().getDisplayName()));
         lore.add("");
         if (hasUnlocked(player)) {
             if (player.getActiveCosmetics().get(getType()) != null) {
