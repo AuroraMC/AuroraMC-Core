c13ab197eb0e43c591c451d73d09fbe4ddb23ab1
Block2Block <accounts@block2block.me>
2023-01-03 17:27:30 +0000

    Load name for report
    Signed-off-by: Block2Block <accounts@block2block.me>

--- src/main/java/net/auroramc/core/api/backend/database/DatabaseManager.java
+++ src/main/java/net/auroramc/core/api/backend/database/DatabaseManager.java
@@ -2408,7 +2408,14 @@ public class DatabaseManager {
                 ResultSet nameSet = statement.executeQuery();
                 nameSet.next();
                 String name = nameSet.getString(1);
-                return new PlayerReport(set.getInt(1), set.getInt(12), name, new ArrayList<>(Arrays.stream(set.getString(2).split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList())), set.getLong(3), PlayerReport.ReportType.valueOf(set.getString(4)), ((set.getString(5) == null)?null: PlayerReport.ChatType.valueOf(set.getString(5))), PlayerReport.ReportReason.valueOf(set.getString(6)), set.getInt(7), null, PlayerReport.ReportOutcome.valueOf(set.getString(8)), ((set.getString(10) == null)?null: PlayerReport.ReportReason.valueOf(set.getString(10))), (PlayerReport.QueueType.valueOf(set.getString(11))), ((set.getString(9) == null)?null:UUID.fromString(set.getString(9))));
+
+                statement = connection.prepareStatement("SELECT name FROM auroramc_players WHERE id = ?");
+                statement.setInt(1, set.getInt(7));
+
+                ResultSet name2Set = statement.executeQuery();
+                name2Set.next();
+                String name2 = name2Set.getString(1);
+                return new PlayerReport(set.getInt(1), set.getInt(12), name, new ArrayList<>(Arrays.stream(set.getString(2).split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList())), set.getLong(3), PlayerReport.ReportType.valueOf(set.getString(4)), ((set.getString(5) == null)?null: PlayerReport.ChatType.valueOf(set.getString(5))), PlayerReport.ReportReason.valueOf(set.getString(6)), set.getInt(7), name2, PlayerReport.ReportOutcome.valueOf(set.getString(8)), ((set.getString(10) == null)?null: PlayerReport.ReportReason.valueOf(set.getString(10))), (PlayerReport.QueueType.valueOf(set.getString(11))), ((set.getString(9) == null)?null:UUID.fromString(set.getString(9))));
             }
             return null;
         } catch (SQLException e) {
