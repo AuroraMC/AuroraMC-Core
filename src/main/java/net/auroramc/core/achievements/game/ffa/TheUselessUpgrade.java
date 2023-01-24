d03f0680be465fa4cd352b295dfd929016a1f82f
Heliology <burntcbusiness@gmail.com>
2023-01-24 22:06:18 +0000

    Fix theuseless upgrade achievement not appearing in GUI

--- src/main/java/net/auroramc/core/achievements/game/ffa/TheUselessUpgrade.java
+++ src/main/java/net/auroramc/core/achievements/game/ffa/TheUselessUpgrade.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class TheUselessUpgrade extends Achievement {
     public TheUselessUpgrade() {
-        super(149, "The Useless Upgrade", "Reach FFA Kit Level 100.", ";&a5000 XP;&65000 Crowns", true, false, AchievementCategory.GAME, 1025);
+        super(149, "The Useless Upgrade", "Reach FFA Kit Level 100.", ";&a5000 XP;&65000 Crowns", true, false, AchievementCategory.GAME, 102);
     }
 }
