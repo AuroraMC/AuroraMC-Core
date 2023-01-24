8a3d8a7c095319e189a242b30ae64ba79295afa1
Heliology <burntcbusiness@gmail.com>
2023-01-24 18:28:47 +0000

    Testing whether colours work

--- src/main/java/net/auroramc/core/achievements/general/Welcome.java
+++ src/main/java/net/auroramc/core/achievements/general/Welcome.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class Welcome extends Achievement {
     public Welcome() {
-        super(0, "Welcome to AuroraMC", "Join the network for the first time!", "None", true, false, AchievementCategory.GENERAL);
+        super(0, "Welcome to AuroraMC", "Join the network for the first time!", "&a100 XP;&6100 Crowns", true, false, AchievementCategory.GENERAL);
     }
 }
