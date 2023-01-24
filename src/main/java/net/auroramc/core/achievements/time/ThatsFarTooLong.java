93741f720d0eaa45541ff006204854dd3147e918
Heliology <burntcbusiness@gmail.com>
2023-01-24 22:10:00 +0000

    Wrong rewards displayed.

--- src/main/java/net/auroramc/core/achievements/time/ThatsFarTooLong.java
+++ src/main/java/net/auroramc/core/achievements/time/ThatsFarTooLong.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class ThatsFarTooLong extends Achievement {
     public ThatsFarTooLong() {
-        super(49, "That's far too long", "Spend two weeks on AuroraMC", ";&a50000 XP;&d50000 Tickets", true, false, AchievementCategory.TIME);
+        super(49, "That's far too long", "Spend two weeks on AuroraMC", ";&a100000 XP;&d100000 Tickets", true, false, AchievementCategory.TIME);
     }
 }
