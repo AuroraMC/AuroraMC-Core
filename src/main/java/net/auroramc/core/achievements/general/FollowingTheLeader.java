eb51de2ec0ef2eb95c0aa8879c3e5e476e9d3b24
Heliology <burntcbusiness@gmail.com>
2023-01-24 19:48:37 +0000

    A tonne of achievement stuff

--- src/main/java/net/auroramc/core/achievements/general/FollowingTheLeader.java
+++ src/main/java/net/auroramc/core/achievements/general/FollowingTheLeader.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class FollowingTheLeader extends Achievement {
     public FollowingTheLeader() {
-        super(26, "Following the Leader", "Be in the same lobby as a member of the Leadership Team", "None", true, false, AchievementCategory.GENERAL);
+        super(26, "Following the Leader", "Be in the same lobby as a member of the Leadership Team", ";&a2000 XP;&d2000 Tickets", true, false, AchievementCategory.GENERAL);
     }
 }
