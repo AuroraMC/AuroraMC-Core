0ecd4c40592dd7679949f9593bf4e4f9882adb05
Heliology <burntcbusiness@gmail.com>
2023-01-24 19:48:37 +0000

    A tonne of achievement stuff

--- src/main/java/net/auroramc/core/achievements/general/Helper.java
+++ src/main/java/net/auroramc/core/achievements/general/Helper.java
@@ -8,6 +8,6 @@ import net.auroramc.core.api.stats.Achievement;
 
 public class Helper extends Achievement {
     public Helper() {
-        super(10, "Helper", "Submit a report that gets accepted", "None", true, false, AchievementCategory.GENERAL);
+        super(10, "Helper", "Submit a report that gets accepted", ";&a500 XP;&6500 Crowns", true, false, AchievementCategory.GENERAL);
     }
 }
