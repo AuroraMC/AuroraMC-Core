43563942cf7134bc6e7d2c60bab3a8579548dff9
Heliology <burntcbusiness@gmail.com>
2023-01-29 20:17:10 +0000

    More experimenting

--- src/main/java/net/auroramc/core/cosmetics/projectiletrails/PurpleTrail.java
+++ src/main/java/net/auroramc/core/cosmetics/projectiletrails/PurpleTrail.java
@@ -33,7 +33,7 @@ public class PurpleTrail extends ProjectileTrail {
                     this.cancel();
                     return;
                 }
-                PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.PORTAL, false, (float) projectile.getLocation().getX(), (float) projectile.getLocation().getY(), (float) projectile.getLocation().getZ(), 0, 0, 0, 1, 5);
+                PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles(EnumParticle.PORTAL, false, (float) projectile.getLocation().getX(), (float) projectile.getLocation().getY(), (float) projectile.getLocation().getZ(), 0, 0, 0, 1, 0);
                 for (AuroraMCPlayer pl : AuroraMCAPI.getPlayers()) {
                     if (pl.getPlayer().getWorld().equals(projectile.getWorld())) {
                         ((CraftPlayer) pl.getPlayer().getPlayer()).getHandle().playerConnection.sendPacket(packet);
