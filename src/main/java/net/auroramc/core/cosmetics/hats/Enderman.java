cf3e1093517c497ee9673c0da71da88b3e997448
Heliology <burntcbusiness@gmail.com>
2023-01-31 22:03:17 +0000

    Fix 3 typos, forgot to change ID on one

--- src/main/java/net/auroramc/core/cosmetics/hats/Enderman.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/Enderman.java
@@ -13,6 +13,6 @@ import java.util.List;
 
 public class Enderman extends Hat {
     public Enderman() {
-        super(361, "Enderman", "&5Enderman", "Become an enderman.", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the End Bundle at store.auroramc.net to unlock this Particle Effect!", "96c0b36d53fff69a49c7d6f3932f2b0fe948e032226d5e8045ec58408a36e951", true, Rarity.EPIC);
+        super(361, "Enderman", "&5Enderman", "Become an enderman.", UnlockMode.STORE_PURCHASE, -1, Collections.emptyList(), Collections.emptyList(), "Purchase the End Bundle at store.auroramc.net to unlock this Hat!", "96c0b36d53fff69a49c7d6f3932f2b0fe948e032226d5e8045ec58408a36e951", true, Rarity.EPIC);
     }
 }
