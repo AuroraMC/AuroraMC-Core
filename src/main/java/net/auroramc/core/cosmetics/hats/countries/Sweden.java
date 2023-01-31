322004c937e49e39e14b5167922ac7b6d226b52e
Heliology <burntcbusiness@gmail.com>
2023-01-31 21:54:51 +0000

    Add new hats (A whole 15 of them), move others into different folder

---
+++ src/main/java/net/auroramc/core/cosmetics/hats/countries/Sweden.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.core.cosmetics.hats.countries;
+
+import net.auroramc.core.api.cosmetics.Cosmetic;
+import net.auroramc.core.api.cosmetics.Hat;
+
+import java.util.Collections;
+
+public class Sweden extends Hat {
+
+    public Sweden() {
+        super(353, "Sweden", "&9&lSwe&e&lden", "", Cosmetic.UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", "7d86242b0d97ece9994660f3974d72df7b887f630a4530dadc5b1ab7c2134aec", true, Cosmetic.Rarity.COMMON);
+    }
+}
