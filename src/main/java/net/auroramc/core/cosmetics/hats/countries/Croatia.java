c3c30c4799117adba160b3267176bfa8fa648957
Heliology <burntcbusiness@gmail.com>
2023-01-31 22:11:30 +0000

    Hats.

--- src/main/java/net/auroramc/core/cosmetics/hats/countries/Croatia.java
+++ src/main/java/net/auroramc/core/cosmetics/hats/countries/Croatia.java
@@ -12,6 +12,6 @@ import java.util.Collections;
 public class Croatia extends Hat {
 
     public Croatia() {
-        super(360, "Croatia", "&c&lCr&9&loa&f&ltia", "", Cosmetic.UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", "b050c04ec8cabce71d7103f3e9ef4bb8819f9f365eb335a9139912bc07ed445", true, Cosmetic.Rarity.COMMON);
+        super(360, "Croatia", "&c&lCr&9&loa&f&ltia", "", UnlockMode.CRATE, -1, Collections.emptyList(), Collections.emptyList(), "Found in Crates", "b050c04ec8cabce71d7103f3e9ef4bb8819f9f365eb335a9139912bc07ed445", true, Cosmetic.Rarity.COMMON);
     }
 }
