fd01c23bf1a1699eb890a366a7308c010f937eed
Block2Block <accounts@block2block.me>
2023-05-24 19:41:14 +0100

    Fixes
    Signed-off-by: Block2Block <accounts@block2block.me>

--- smp/src/main/java/net/auroramc/core/commands/general/CommandHome.java
+++ smp/src/main/java/net/auroramc/core/commands/general/CommandHome.java
@@ -2,18 +2,18 @@
  * Copyright (c) 2023 AuroraMC Ltd. All Rights Reserved.
  */
 
-package net.auroramc.core.commands.general.team;
+package net.auroramc.core.commands.general;
 
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
 import net.auroramc.api.AuroraMCAPI;
 import net.auroramc.api.backend.info.ServerInfo;
+import net.auroramc.api.permissions.Permission;
 import net.auroramc.api.utils.SMPLocation;
 import net.auroramc.api.utils.TextFormatter;
 import net.auroramc.core.api.ServerAPI;
 import net.auroramc.core.api.ServerCommand;
 import net.auroramc.core.api.player.AuroraMCServerPlayer;
-import net.auroramc.core.api.player.SMPPlayer;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -22,39 +22,38 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
-public class CommandTeamHome extends ServerCommand {
+public class CommandHome extends ServerCommand {
 
 
-    public CommandTeamHome() {
-        super("home", Collections.emptyList(), Collections.emptyList(), true, null);
+    public CommandHome() {
+        super("home", Collections.emptyList(), Collections.singletonList(Permission.PLAYER), false, null);
     }
 
     @Override
     public void execute(AuroraMCServerPlayer player, String aliasUsed, List<String> args) {
-        if (player.getSmpTeam() != null) {
-            if (player.getSmpTeam().getHome() != null) {
+        if (player.getHome() != null) {
+            if (player.getSmpTeam() == null) {
                 switch (((ServerInfo) AuroraMCAPI.getInfo()).getServerType().getString("smp_type")) {
                     case "OVERWORLD": {
-                        if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.OVERWORLD) {
-                            SMPLocation location = player.getSmpTeam().getHome();
+                        if (player.getHome().getDimension() == SMPLocation.Dimension.OVERWORLD) {
+                            SMPLocation location = player.getHome();
                             player.teleport(new Location(Bukkit.getWorld("smp"), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch()));
                         } else {
-                            new BukkitRunnable(){
+                            new BukkitRunnable() {
                                 @Override
                                 public void run() {
                                     ByteArrayDataOutput out = ByteStreams.newDataOutput();
-                                    if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.END) {
+                                    if (player.getHome().getDimension() == SMPLocation.Dimension.END) {
                                         out.writeUTF("SMPEnd");
-                                    } else if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.NETHER) {
+                                    } else if (player.getHome().getDimension() == SMPLocation.Dimension.NETHER) {
                                         out.writeUTF("SMPNether");
                                     } else {
                                         return;
                                     }
                                     out.writeUTF(player.getUniqueId().toString());
                                     player.saveData();
-                                    AuroraMCAPI.getDbManager().setSMPLogoutLocation(player.getUniqueId(), new SMPLocation(player.getSmpTeam().getHome().getDimension(), player.getSmpTeam().getHome().getX(), player.getSmpTeam().getHome().getY(), player.getSmpTeam().getHome().getZ(), player.getSmpTeam().getHome().getPitch(), player.getSmpTeam().getHome().getYaw(), SMPLocation.Reason.HOME));
+                                    AuroraMCAPI.getDbManager().setSMPLogoutLocation(player.getUniqueId(), new SMPLocation(player.getHome().getDimension(), player.getHome().getX(), player.getHome().getY(), player.getHome().getZ(), player.getHome().getPitch(), player.getHome().getYaw(), SMPLocation.Reason.HOME));
                                     player.sendPluginMessage(out.toByteArray());
                                 }
                             }.runTaskAsynchronously(ServerAPI.getCore());
@@ -62,24 +61,24 @@ public class CommandTeamHome extends ServerCommand {
                         break;
                     }
                     case "NETHER": {
-                        if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.NETHER) {
-                            SMPLocation location = player.getSmpTeam().getHome();
+                        if (player.getHome().getDimension() == SMPLocation.Dimension.NETHER) {
+                            SMPLocation location = player.getHome();
                             player.teleport(new Location(Bukkit.getWorld("smp"), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch()));
                         } else {
-                            new BukkitRunnable(){
+                            new BukkitRunnable() {
                                 @Override
                                 public void run() {
                                     ByteArrayDataOutput out = ByteStreams.newDataOutput();
-                                    if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.END) {
+                                    if (player.getHome().getDimension() == SMPLocation.Dimension.END) {
                                         out.writeUTF("SMPEnd");
-                                    } else if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.OVERWORLD) {
+                                    } else if (player.getHome().getDimension() == SMPLocation.Dimension.OVERWORLD) {
                                         out.writeUTF("SMPOverworld");
                                     } else {
                                         return;
                                     }
                                     out.writeUTF(player.getUniqueId().toString());
                                     player.saveData();
-                                    AuroraMCAPI.getDbManager().setSMPLogoutLocation(player.getUniqueId(), new SMPLocation(player.getSmpTeam().getHome().getDimension(), player.getSmpTeam().getHome().getX(), player.getSmpTeam().getHome().getY(), player.getSmpTeam().getHome().getZ(), player.getSmpTeam().getHome().getPitch(), player.getSmpTeam().getHome().getYaw(), SMPLocation.Reason.HOME));
+                                    AuroraMCAPI.getDbManager().setSMPLogoutLocation(player.getUniqueId(), new SMPLocation(player.getHome().getDimension(), player.getHome().getX(), player.getHome().getY(), player.getHome().getZ(), player.getHome().getPitch(), player.getHome().getYaw(), SMPLocation.Reason.HOME));
                                     player.sendPluginMessage(out.toByteArray());
                                 }
                             }.runTaskAsynchronously(ServerAPI.getCore());
@@ -87,24 +86,24 @@ public class CommandTeamHome extends ServerCommand {
                         break;
                     }
                     case "END": {
-                        if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.END) {
-                            SMPLocation location = player.getSmpTeam().getHome();
+                        if (player.getHome().getDimension() == SMPLocation.Dimension.END) {
+                            SMPLocation location = player.getHome();
                             player.teleport(new Location(Bukkit.getWorld("smp"), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch()));
                         } else {
-                            new BukkitRunnable(){
+                            new BukkitRunnable() {
                                 @Override
                                 public void run() {
                                     ByteArrayDataOutput out = ByteStreams.newDataOutput();
-                                    if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.OVERWORLD) {
+                                    if (player.getHome().getDimension() == SMPLocation.Dimension.OVERWORLD) {
                                         out.writeUTF("SMPOverworld");
-                                    } else if (player.getSmpTeam().getHome().getDimension() == SMPLocation.Dimension.NETHER) {
+                                    } else if (player.getHome().getDimension() == SMPLocation.Dimension.NETHER) {
                                         out.writeUTF("SMPNether");
                                     } else {
                                         return;
                                     }
                                     out.writeUTF(player.getUniqueId().toString());
                                     player.saveData();
-                                    AuroraMCAPI.getDbManager().setSMPLogoutLocation(player.getUniqueId(), new SMPLocation(player.getSmpTeam().getHome().getDimension(), player.getSmpTeam().getHome().getX(), player.getSmpTeam().getHome().getY(), player.getSmpTeam().getHome().getZ(), player.getSmpTeam().getHome().getPitch(), player.getSmpTeam().getHome().getYaw(), SMPLocation.Reason.HOME));
+                                    AuroraMCAPI.getDbManager().setSMPLogoutLocation(player.getUniqueId(), new SMPLocation(player.getHome().getDimension(), player.getHome().getX(), player.getHome().getY(), player.getHome().getZ(), player.getHome().getPitch(), player.getHome().getYaw(), SMPLocation.Reason.HOME));
                                     player.sendPluginMessage(out.toByteArray());
                                 }
                             }.runTaskAsynchronously(ServerAPI.getCore());
@@ -113,12 +112,13 @@ public class CommandTeamHome extends ServerCommand {
                     }
                 }
             } else {
-                player.sendMessage(TextFormatter.pluginMessage("Teams", "Your team does not have a home set, the leader of the team needs to use **/sethome** to set the team home."));
+                player.sendMessage(TextFormatter.pluginMessage("Home", "You are currently in a team, so do not have a home. Use **/team home** to go to your team home."));
             }
         } else {
-            player.sendMessage(TextFormatter.pluginMessage("Teams", "You must be in a team to use this command."));
+            player.sendMessage(TextFormatter.pluginMessage("Home", "You don't have a home set. Set a home using **/sethome**."));
         }
     }
+    
 
     @Override
     public @NotNull List<String> onTabComplete(AuroraMCServerPlayer player, String aliasUsed, List<String> args, String lastToken, int numberArguments) {
