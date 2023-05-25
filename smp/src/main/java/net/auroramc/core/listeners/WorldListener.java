ef48fccdb5be1227208e88fe8aebd22fd815b501
Block2Block <accounts@block2block.me>
2023-05-26 00:52:56 +0100

    Some changes
    Signed-off-by: Block2Block <accounts@block2block.me>

--- smp/src/main/java/net/auroramc/core/listeners/WorldListener.java
+++ smp/src/main/java/net/auroramc/core/listeners/WorldListener.java
@@ -68,6 +68,7 @@ public class WorldListener implements Listener {
         smp.setGameRule(GameRule.DO_FIRE_TICK, false);
         smp.setGameRule(GameRule.KEEP_INVENTORY, true);
         smp.setGameRule(GameRule.DO_INSOMNIA, false);
+        smp.setDifficulty(Difficulty.HARD);
 
         AuroraMCAPI.getLogger().info("AuroraMC-SMP loaded and ready to accept connections. Letting mission control know...");
         ProtocolMessage message = new ProtocolMessage(Protocol.SERVER_ONLINE, "Mission Control", "", AuroraMCAPI.getInfo().getName(), AuroraMCAPI.getInfo().getNetwork().name());
