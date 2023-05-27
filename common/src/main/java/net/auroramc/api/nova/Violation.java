353344f2d3100249ede8b72a4ffceeb710520b87
Block2Block <accounts@block2block.me>
2023-05-27 19:36:39 +0100

    Add reloading
    Signed-off-by: Block2Block <accounts@block2block.me>

---
+++ common/src/main/java/net/auroramc/api/nova/Violation.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023 AuroraMC Ltd. All Rights Reserved.
+ */
+
+package net.auroramc.api.nova;
+
+public class Violation {
+
+    private long timestamp;
+    private NovaCheck check;
+
+}
