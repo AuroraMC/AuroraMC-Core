8341a27dd7bea7b0c87aef1672bf75e329a1514b
Block2Block <accounts@block2block.me>
2023-06-16 15:10:39 +0100

    Update protocol
    Signed-off-by: Block2Block <accounts@block2block.me>

--- smp/src/main/java/net/auroramc/core/api/backend/communication/IncomingProtocolMessageThread.java
+++ smp/src/main/java/net/auroramc/core/api/backend/communication/IncomingProtocolMessageThread.java
@@ -4,7 +4,7 @@
  * PRIVATE AND CONFIDENTIAL - Distribution and usage outside the scope of your job description is explicitly forbidden except in circumstances where a company director has expressly given written permission to do so.
  */
 
-package net.auroramc.smp.api.backend.communication;
+package net.auroramc.core.api.backend.communication;
 
 import net.auroramc.api.AuroraMCAPI;
 import net.auroramc.smp.api.ServerAPI;
